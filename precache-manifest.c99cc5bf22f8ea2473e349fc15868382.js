9d53ffa54b2b1bff5e4d4583e825c1667fb97aac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-21 00:56:37 +0000

    Deploy itsjafer/itsjafer.github.io to github.com/itsjafer/itsjafer.github.io.git:master

---
+++ precache-manifest.c99cc5bf22f8ea2473e349fc15868382.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8099f3baec5ef391b030c14d85b42162",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d95dd6adc75d04b110bf",
+    "url": "/static/css/2.7a337cc1.chunk.css"
+  },
+  {
+    "revision": "ad76828e29bf645537b6",
+    "url": "/static/css/main.e4db3e86.chunk.css"
+  },
+  {
+    "revision": "d95dd6adc75d04b110bf",
+    "url": "/static/js/2.8d8b5485.chunk.js"
+  },
+  {
+    "revision": "2db34d1d4bdd00f5e54e8de5d8a929c1",
+    "url": "/static/js/2.8d8b5485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad76828e29bf645537b6",
+    "url": "/static/js/main.b46cde1c.chunk.js"
+  },
+  {
+    "revision": "447edeee62eb2871bcd0",
+    "url": "/static/js/runtime-main.c9a36f42.js"
+  },
+  {
+    "revision": "5c36e271ad6a624e79f69c069fe1a1e2",
+    "url": "/static/media/BU362.5c36e271.md"
+  },
+  {
+    "revision": "13c1fcc1048167d48e221174bbf47765",
+    "url": "/static/media/CS240.13c1fcc1.md"
+  },
+  {
+    "revision": "e34bf6160e2d9f9e0d6de3c624ba1bf5",
+    "url": "/static/media/CS341.e34bf616.md"
+  }
+]);
